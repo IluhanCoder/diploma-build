eeb6d1b82387e3fc67f047c78283bc417d501c7c
IluhanCoder <Komunx324@gmail.com>
2023-03-10 20:16:50 +0200

    fixed cors

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export const API_URL = "https://music-web-server.onrender.com";
+export const API_URL = "https://music-web-server.onrender.com/api";
 
 const $api = axios.create({
   withCredentials: true,
