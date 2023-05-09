cd29c5926071f40857b1985cdc2424e69a109122
ILya Coder <90256163+IluhanCoder@users.noreply.github.com>
2023-05-09 17:39:04 +0300

    Update invite-service.js

--- server/service/invite-service.js
+++ server/service/invite-service.js
@@ -62,7 +62,7 @@ class InviteService {
     if (accept) {
       const convertedReceiverId = Mongoose.Types.ObjectId(invite.receiverId);
       await eventModel.updateOne(
-        { _id: invite.eventId.toString() },
+        { _id: Mongoose.Types.ObjectId(invite.eventId) },,
         {
           $push: {
             participants: {
